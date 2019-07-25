fa34578360f1210aa35e561caa4ba398d9b2962e
Joe Gregorio <jcgregorio@google.com>
2019-07-25 11:21:59 -0400

    Update some styles.
    Make --red look redder.
    Make the drop shadow on buttons smaller.
    Fix the location for styles in their @modules descriptions.

--- elements-sk/styles/buttons/index.js
+++ elements-sk/styles/buttons/index.js
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-/** @module elements-sk/buttons
+/** @module elements-sk/styles/buttons
  *
  * @description CSS Styling for native buttons.
  * @example
--- elements-sk/styles/select/index.js
+++ elements-sk/styles/select/index.js
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-/** @module elements-sk/select
+/** @module elements-sk/styles/select
  *
  * @description CSS Styling for native select and options elements.
  */
