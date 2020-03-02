e452040443163c479e8ee4b47cc1257bb42229ed
Joe Gregorio <jcgregorio@google.com>
2020-03-02 12:06:18 -0800

    Also handle message.resp in errorMessage. (#11)
    common-sk/jsonOrThrow will return a window.Response object in
    the message, which we can use to extract the HTTP response text and
    use that as the message.

--- elements-sk/errorMessage.js
+++ elements-sk/errorMessage.js
@@ -1,6 +1,7 @@
 /** errorMessage dispatches an event with the error message in it.
  * <p>
- *   Use this function in conjunction with the {@linkcode module:elements-sk/error-toast-sk} element.
+ *   Use this function in conjunction with the
+ *   {@linkcode module:elements-sk/error-toast-sk} element.
  * </p>
  *
  * @evt error-sk The event has a detail of the form:
@@ -13,19 +14,22 @@
  * </pre>
  *
  * @param {(string|Object)} message The value of 'message' is expected to be a
- *   string, an Object with a field 'message', or an Object.
- * @param {number} duration The number of milliseconds the message should be displayed.
+ *   string, an Object with a field 'message', an Object with a field 'resp'
+ *   that is an instance of window.Response, or an Object.
+ * @param {number} duration The number of milliseconds the message should be
+ * displayed.
  *
  */
-export function errorMessage(message, duration=10000) {
-  if (typeof message === 'object') {
-    message = message.message        || // for handling Errors {name:String, message:String}
-              JSON.stringify(message);  // for everything else
+export async function errorMessage(message, duration = 10000) {
+  if (message.resp instanceof window.Response) {
+    message = await message.resp.text();
+  } else if (typeof message === 'object') {
+    message = message.message // for handling Errors {name:String, message:String}
+              || JSON.stringify(message); // for everything else
   }
-  var detail = {
+  const detail = {
     message: message,
     duration: duration,
-  }
-  document.dispatchEvent(new CustomEvent('error-sk', {detail: detail, bubbles: true}));
+  };
+  document.dispatchEvent(new CustomEvent('error-sk', { detail: detail, bubbles: true }));
 }
-
